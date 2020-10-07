aa525ad3e5c55525c39afe2df2ce1d58fac6e266
poojasingh3548 <poojadurgesh1999@gmail.com>
2020-10-07 06:10:04 +0000

    n

---
+++ Prime.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+
+
+int Is_Prime(int num)
+{
+  for (i=2;i<num;i++)
+  {
+    
+  }
+}
+
+int main()
+{
+  int num;
+  cout<<"Enter a number ";
+  cin>>num;
+  result=Is_Prime(num);
+  cout>>result;
+
+  return 0;
+}
+
